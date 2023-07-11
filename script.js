b1bd3f5390794e7850879cf525f1240f23b94ad1
rimi <a.fatimaarimi@gmail.com>
2023-07-11 14:02:43 -0400

    created person class

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HAMSTER
 class Hamster {
   constructor(owner, name, price) {
     this.owner = owner;
@@ -16,5 +17,70 @@ class Hamster {
   }
   getPrice() {
     return price;
+  }
+}
+
+const h1 = new Hamster ("Mo", "Momo", true);
+
+console.log(h1);
+h1.wheelRun()
+h1.eatFood()
+h1.getPrice()
+if (h1 === 15) {
+    console.log(`${price}`);
+} else {
+    console.log(`${this.name} NOT FOR SALE!`);
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// PERSON
+
+class Person {
+  constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+    this.name = name;
+
+    this.age = age;
+    this.age = 0;
+
+    this.height = height;
+    this.height = 0;
+
+    this.weight = weight;
+    this.weight = 0;
+
+    this.mood = mood;
+    this.mood = 0;
+
+    this.hamster = hamsters;
+    this.hamsters = [];
+
+    this.bankAccount = bankAccount;
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return age;
+  }
+  getWeight() {
+    return weight;
+  }
+  greet() {
+    console.log(this.name);
+  }
+  eat() {
+    weight++, mood++;
+  }
+  exercise() {
+    weight--;
+  }
+  ageUp() {
+    age++, height++, weight++, mood--, this.bankAccount + 10;
+  }
+  buyHamster(hamster) {
+    hamster.push;
+    mood + 10, bankAccount - getPrice();
   }
 }
