5dc0fa1c14f0840e2a584da668c62f179ef24e8c
rimi <a.fatimaarimi@gmail.com>
2023-07-11 12:54:57 -0400

    created hamster class

---
+++ script.js
@@ -0,0 +1,20 @@
+class Hamster {
+  constructor(owner, name, price) {
+    this.owner = owner;
+    this.owner = "";
+
+    this.name = name;
+
+    this.price = price;
+    this.price = 15;
+  }
+  wheelRun() {
+    console.log("squeak squeak");
+  }
+  eatFood() {
+    console.log("nibble nibble");
+  }
+  getPrice() {
+    return price;
+  }
+}
